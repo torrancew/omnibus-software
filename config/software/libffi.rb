029b9f92f304fe69aaef10a409a7fa23391829bd
Lamont Granquist <lamont@scriptkiddie.org>
2013-11-12 12:45:27 -0800

    <rage>single vs double quotes</rage>

--- config/software/libffi.rb
+++ config/software/libffi.rb
@@ -24,7 +24,7 @@ dependency "libgcc"
 #       we'll need to use a more robust link (sourceforge) that will
 #       not change over time.
 source :url => "ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz",
-       :md5 => '45f3b6dbc9ee7c7dfbbbc5feba571529"
+       :md5 => '45f3b6dbc9ee7c7dfbbbc5feba571529'
 
 relative_path "libffi-3.0.13"
 configure_env =
