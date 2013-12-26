019a006b88038d715f596d845fead84991935dc3
Tray Torrance <tray@artemis.net>
2013-12-26 02:06:12 -0800

    Build ruby 2.1.0

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -15,46 +15,47 @@
 # limitations under the License.
 #
 
-name "ruby"
-version "1.9.3-p484"
+name    'ruby'
+version '2.1.0'
 
-dependency "zlib"
-dependency "ncurses"
-dependency "libedit"
-dependency "openssl"
-dependency "libyaml"
-dependency "libiconv"
-dependency "gdbm" if (platform == "mac_os_x" or platform == "freebsd" or platform == "aix")
-dependency "libgcc" if (platform == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
+dependency 'zlib'
+dependency 'ncurses'
+dependency 'libedit'
+dependency 'openssl'
+dependency 'libyaml'
+dependency 'libiconv'
 
-source :url => "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-#{version}.tar.gz",
-       :md5 => '8ac0dee72fe12d75c8b2d0ef5d0c2968'
+dependency 'gdbm'   if (platform == 'mac_os_x' or platform == 'freebsd' or platform == 'aix')
+dependency 'libgcc' if (platform == 'solaris2' and Omnibus.config.solaris_compiler == 'gcc')
+
+source :url => "http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-#{version}.tar.gz",
+       :md5 => '9e6386d53f5200a3e7069107405b93f7'
 
 relative_path "ruby-#{version}"
 
 env =
   case platform
-  when "mac_os_x"
+  when 'mac_os_x'
     {
-      "CFLAGS" => "-arch x86_64 -m64 -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -I#{install_dir}/embedded/include/ncurses -O3 -g -pipe",
+      "CFLAGS"  => "-arch x86_64 -m64 -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -I#{install_dir}/embedded/include/ncurses -O3 -g -pipe",
       "LDFLAGS" => "-arch x86_64 -R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -I#{install_dir}/embedded/include/ncurses"
     }
-  when "solaris2"
+  when 'solaris2'
     if Omnibus.config.solaris_compiler == "studio"
     {
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
+      'CFLAGS'  => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      'LDFLAGS' => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
     }
     elsif Omnibus.config.solaris_compiler == "gcc"
     {
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -O3 -g -pipe",
-      "LDFLAGS" => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-      "LD_OPTIONS" => "-R#{install_dir}/embedded/lib"
+      'CFLAGS'     => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -O3 -g -pipe",
+      'LDFLAGS'    => "-R#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
+      'LD_OPTIONS' => "-R#{install_dir}/embedded/lib"
     }
     else
       raise "Sorry, #{Omnibus.config.solaris_compiler} is not a valid compiler selection."
     end
-  when "aix"
+  when 'aix'
     {
       # see http://www.ibm.com/developerworks/aix/library/au-gnu.html
       #
@@ -79,55 +80,55 @@ env =
       # We also need prezl's M4 instead of picking up /usr/bin/m4 which
       # barfs on ruby.
       #
-      "CC" => "xlc -q64",
-      "CXX" => "xlC -q64",
-      "LD" => "ld -b64",
-      "CFLAGS" => "-q64 -O -qhot -I#{install_dir}/embedded/include",
-      "CXXFLAGS" => "-q64 -O -qhot -I#{install_dir}/embedded/include",
-      "LDFLAGS" => "-q64  -L#{install_dir}/embedded/lib -Wl,-brtl -Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib",
-      "OBJECT_MODE" => "64",
-      "ARFLAGS" => "-X64 cru",
-      "M4" => "/opt/freeware/bin/m4",
-      "warnflags" => "-qinfo=por"
+      'CC'          => 'xlc -q64',
+      "CXX"         => 'xlC -q64',
+      'LD'          => 'ld -b64',
+      'CFLAGS'      => "-q64 -O -qhot -I#{install_dir}/embedded/include",
+      'CXXFLAGS'    => "-q64 -O -qhot -I#{install_dir}/embedded/include",
+      'LDFLAGS'     => "-q64  -L#{install_dir}/embedded/lib -Wl,-brtl -Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib",
+      'OBJECT_MODE' => '64',
+      'ARFLAGS'     => '-X64 cru',
+      'M4'          => '/opt/freeware/bin/m4',
+      'warnflags'   => '-qinfo=por'
     }
   else
     {
-      "CFLAGS" => "-I#{install_dir}/embedded/include -O3 -g -pipe",
-      "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib"
+      'CFLAGS'  => "-I#{install_dir}/embedded/include -O3 -g -pipe",
+      'LDFLAGS' => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib"
     }
   end
 
 build do
-  configure_command = ["./configure",
+  configure_command = ['./configure',
                        "--prefix=#{install_dir}/embedded",
-                       "--with-out-ext=fiddle",
-                       "--enable-shared",
-                       "--enable-libedit",
-                       "--with-ext=psych",
-                       "--disable-install-doc"]
+                       '--with-out-ext=fiddle',
+                       '--enable-shared',
+                       '--enable-libedit',
+                       '--with-ext=psych',
+                       '--disable-install-doc']
 
   case platform
-  when "aix"
-    patch :source => "ruby-aix-configure.patch", :plevel => 1
-    patch :source => "ruby_aix_1_9_3_448_ssl_EAGAIN.patch", :plevel => 1
+  when 'aix'
+    patch :source => 'ruby-aix-configure.patch', :plevel => 1
+    patch :source => 'ruby_aix_1_9_3_448_ssl_EAGAIN.patch', :plevel => 1
     # --with-opt-dir causes ruby to send bogus commands to the AIX linker
-  when "freebsd"
-    configure_command << "--without-execinfo"
+  when 'freebsd'
+    configure_command << '--without-execinfo'
     configure_command << "--with-opt-dir=#{install_dir}/embedded"
-  when "smartos"
+  when 'smartos'
     # Opscode patch - someara@opscode.com
     # GCC 4.7.0 chokes on mismatched function types between OpenSSL 1.0.1c and Ruby 1.9.3-p286
-    patch :source => "ruby-openssl-1.0.1c.patch", :plevel => 1
+    patch :source => 'ruby-openssl-1.0.1c.patch', :plevel => 1
 
     # Patches taken from RVM.
     # http://bugs.ruby-lang.org/issues/5384
     # https://www.illumos.org/issues/1587
     # https://github.com/wayneeseguin/rvm/issues/719
-    patch :source => "rvm-cflags.patch", :plevel => 1
+    patch :source => 'rvm-cflags.patch', :plevel => 1
 
     # From RVM forum
     # https://github.com/wayneeseguin/rvm/commit/86766534fcc26f4582f23842a4d3789707ce6b96
-    configure_command << "ac_cv_func_dl_iterate_phdr=no"
+    configure_command << 'ac_cv_func_dl_iterate_phdr=no'
     configure_command << "--with-opt-dir=#{install_dir}/embedded"
   else
     configure_command << "--with-opt-dir=#{install_dir}/embedded"
@@ -135,15 +136,15 @@ build do
 
   # @todo expose bundle_bust() in the DSL
   env.merge!({
-    "RUBYOPT"         => nil,
-    "BUNDLE_BIN_PATH" => nil,
-    "BUNDLE_GEMFILE"  => nil,
-    "GEM_PATH"        => nil,
-    "GEM_HOME"        => nil
+    'RUBYOPT'         => nil,
+    'BUNDLE_BIN_PATH' => nil,
+    'BUNDLE_GEMFILE'  => nil,
+    'GEM_PATH'        => nil,
+    'GEM_HOME'        => nil
   })
 
   # @todo: move into omnibus-ruby
-  has_gmake = system("gmake --version")
+  has_gmake = system('gmake --version')
 
   if has_gmake
     env.merge!({'MAKE' => 'gmake'})
@@ -152,7 +153,8 @@ build do
     make_binary = 'make'
   end
 
-  command configure_command.join(" "), :env => env
+  command configure_command.join(' '), :env => env
   command "#{make_binary} -j #{max_build_jobs}", :env => env
   command "#{make_binary} -j #{max_build_jobs} install", :env => env
 end
+
